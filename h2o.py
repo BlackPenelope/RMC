b6aa512b1f297d5f65854685114051a3f87cc689
Morita-T1700 <penelope.hide.morita@gmail.com>
2021-05-31 13:24:36 +0900

    RMCファイルの原子にPDBファイルから読み込んだ分子データを追加しRMCファイルを書き出すプログラム

---
+++ h2o.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 12:04:13 2021
+
+@author: H.Morita
+"""
+
+import rmc_configuration as rmc_cfg
+import pdb_configuration as pdb_cfg
+import rmc_util
+import numpy as np
+
+'''
+
+def rmc2xyz(rmc, atoms, path, title, elec):
+    with open(path, mode='w') as f:
+        
+        f.write("{}\n".format(rmc.nmol))
+        f.write(title)
+        f.write("{:10.06f}\n".format(elec))
+        
+        ni = 0
+        for i in range(rmc.nmol_types):
+            for n in range(ni, ni + rmc.ni[i]):
+                f.write(atoms[i])
+                v = rmc.atoms.positions[n]
+                pos = np.dot(v, rmc.vectors)
+                f.write(" {:8.04f}".format(pos[0]))
+                f.write(" {:8.04f}".format(pos[1]))
+                f.write(" {:8.04f}\n".format(pos[2]))
+                    
+            ni = ni + rmc.ni[i]
+        
+
+rmc = rmc_cfg.RmcConfiguration()
+rmc.read('./sio2_h2o_md.cfg')
+
+rmc2xyz(rmc, ["Si", "Si", "O", "O"], "./sio2.xyz", "SiO2", 0.0)
+rmc2xyz(rmc, ["Si", "Si", "O", "S"], "./sios.xyz", "SiO2", 0.0)
+
+mat = rmc.vectors          # cell matrix
+imat = np.linalg.inv(mat)  # cell matrix inverse
+
+add_nmol_types = 4
+
+pdb_path = 'H2O.pdb'
+pdb = pdb_cfg.PdbConfiguration()
+pdb.read(pdb_path)
+center_idx = 0  # add center index in pdb molecule
+
+c = pdb.atoms.positions[center_idx]
+
+add_vec = []
+for i in range(1, pdb.atoms.number):
+    v = pdb.atoms.positions[i] - c
+    add_vec.append(v)
+
+add_atoms = []
+ni = 0
+for i in range(rmc.nmol_types):
+    for n in range(ni, ni+rmc.ni[i]):
+        v = rmc.atoms.positions[n]
+        #pos = np.dot(v, mat)
+        
+        # add atom rmc index
+        if i == add_nmol_types-1:
+            m = rmc_util.uniform_random_rotate_matrix()
+            pos = np.dot(v, mat)
+            #print(i, v)
+            for j in range(len(add_vec)):
+                
+                add_pos = pos + np.dot(add_vec[j], m)
+                #print(np.linalg.norm(add_pos))
+                add_atoms.append(add_pos)
+                
+    ni = ni + rmc.ni[i]
+
+# update nmol, ni, nsite, nmol_types, sites
+# rmc.atoms.number, positions
+
+add_number = len(add_atoms)
+rmc.nmol = rmc.nmol + add_number
+rmc.ni.append(add_number)
+rmc.nsites.append(1)
+rmc.nmol_types = rmc.nmol_types + 1
+
+#a = np.array([0.,1.,2.,3,4,5]).reshape(2,3)
+#b = np.append(a, [[6,7,8]], axis=0)
+#c = np.array([[6,7,8]])
+#print(c.shape) #(1,3)
+
+for i in range(add_number):
+    pos = np.dot(add_atoms[i], imat)
+    position = np.array([pos])
+    rmc.atoms.positions = np.append(rmc.atoms.positions, position, axis=0)
+
+rmc.atoms.number = rmc.atoms.number + rmc.atoms.positions.shape[0]
+rmc.sites.append([np.array([0., 0., 0.])])
+
+rmc.write("./sio2_add_h2o_md.cfg")
+
+rmc2xyz(rmc, ["Si", "Si", "O", "O", "H"], "./sio2_h2o.xyz", "SiO2H", 0.0)
+'''
