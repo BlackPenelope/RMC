fac41ed87ac03834c0bfb3c374da36552d7b4b06
Morita-T1700 <penelope.hide.morita@gmail.com>
2021-08-04 15:47:30 +0900

    はじめてのQt GUI表示

--- startGUI.py
+++ startGUI.py
@@ -4,6 +4,19 @@ Created on Wed Aug  4 15:31:45 2021
 
 @author: Morita-T1700
 """
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from gui import main_window
+
+#main_window.aaa()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    w.show()
+    sys.exit(app.exec())
 
 if __name__ == '__main__':
-    pass
+    main()
