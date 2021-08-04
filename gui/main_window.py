abccda06d85f7802b1e22d6f53959d1ed4d5e77b
Morita-T1700 <penelope.hide.morita@gmail.com>
2021-08-04 15:48:54 +0900

    MainWindowクラス作成＆追加

---
+++ gui/main_window.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  4 15:34:21 2021
+
+@author: Morita-T1700
+"""
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow):
+    pass
+    
+def aaa():
+    print('ggg')
