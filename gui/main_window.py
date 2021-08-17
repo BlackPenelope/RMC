4085bdc9be2c01aadb423f473d3a0a20ba129fd8
Mac Morita-PC <penelope.hide.morita@gmail.com>
2021-08-17 14:46:07 +0900

    Add Coment

--- gui/main_window.py
+++ gui/main_window.py
@@ -22,7 +22,7 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
         
         #self.center = CentralWidget(self)
-        
+        #
         #self.setCentralWidget(self.center)
                 
         self.statusBar().showMessage('RMC')
